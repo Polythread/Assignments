ea3ec6866ddf654dbb7c97bbf31ceb99c15f0bc6
polythread <mantech468@gmail.com>
2026-09-19 23:09:08 +0530

    Typescript

--- Typescript/src/generics/problem-04.ts
+++ Typescript/src/generics/problem-04.ts
@@ -1,2 +1,6 @@
 // Problem Statement:
 // Write a generic function sum that accepts an array of numbers and returns the sum of all the numbers.
+
+export function sum<T extends number>(arr: T[]): number {
+  return arr.reduce((total, num) => total + num, 0);
+}
