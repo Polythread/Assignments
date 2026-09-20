7356097e04f33450197736aea89e9a3229f6f11f
polythread <mantech468@gmail.com>
2026-09-20 23:03:53 +0530

    Typescript

--- Typescript/src/hard/arrayOne.ts
+++ Typescript/src/hard/arrayOne.ts
@@ -10,4 +10,18 @@
 // Example Input 3: []
 // Example Output 3: Throws an error: "Array cannot be empty"
 
+export function findMinimumValue(arr: number[]): number {
+  if (arr.length === 0) {
+    throw new Error("Array cannot be empty");
+  }
 
+  let min = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  return min;
+}
